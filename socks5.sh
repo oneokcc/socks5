829692ab18713b1709c1cc993a7ade985a38e85d
oneokcc <141986052+oneokcc@users.noreply.github.com>
2024-03-05 07:45:58 +0800

    Update socks5.sh

--- socks5.sh
+++ socks5.sh
@@ -46,16 +46,16 @@ echo "runing pelase waiting"
    PIDS=`ps -ef|grep gost|grep -v grep`
    if [ "$PIDS" != "" ]; then
       s=`ps -ef|grep gost|grep -v grep|awk '{print $2}'| wc -l`
-      echo -e "\033[35m检测到本机共有$v个IP地址，并成功搭建$s条;多ip服务器游戏推荐使用：方式二\033[0m"
+      echo -e "\033[35m检测到本机共有$v个IP地址,并成功生成$s条.\033[0m"
       cat /tmp/s5
       
-      echo -e "\033[33m 是否需要导出所有的配置数据到电脑上？需要请输入 1 ,文件名是 s5 t.txt \033[0m"&&read value
-      if [ $value -eq 1 ]; then
-            yum -y install lrzsz
-            echo -e "\033[41m" 开始导出，请注意文件名是s5 t.txt "\033[0m"
-            sz /tmp/s5
-            echo -e "\033[41m" 请注意，文件名是 s5 t.txt "\033[0m"
-      fi
+     # echo -e "\033[33m 是否需要导出所有的配置数据到电脑上？需要请输入 1 ,文件名是 s5 t.txt \033[0m"&&read value
+     # if [ $value -eq 1 ]; then
+           # yum -y install lrzsz
+            #echo -e "\033[41m" 开始导出，请注意文件名是s5 t.txt "\033[0m"
+            #sz /tmp/s5
+            #echo -e "\033[41m" 请注意，文件名是 s5 t.txt "\033[0m"
+      #fi
       
       
       echo -e "\033[33m  安装完成 \033[0m"&&read -s -n1
